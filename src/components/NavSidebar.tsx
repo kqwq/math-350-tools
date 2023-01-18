43b964fffbf35fc1bbe8ed796020f33330dc2db8
Kyle <kyle@example.com>
2023-01-18 18:06:06 -0500

    navbar

--- src/components/NavSidebar.tsx
+++ src/components/NavSidebar.tsx
@@ -1,11 +1,33 @@
-import { HamburgerIcon } from '@chakra-ui/icons'
-import { Box, Icon, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Input, useDisclosure } from '@chakra-ui/react'
+import { ExternalLinkIcon, HamburgerIcon } from '@chakra-ui/icons'
+import { Box, Icon, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Input, useDisclosure, VStack } from '@chakra-ui/react'
+import Link from 'next/link'
 import React from 'react'
+import { tools } from '../constants'
+
+const Love = () => {
+  // When the user hovers, display the heart emoji, otherwise display &lt;3
+  const love = React.useRef(null)
+  return (
+    <Box
+      ref={love}
+      as="span"
+      onMouseEnter={() => (love.current.innerText = '❤️')}
+      onMouseLeave={() => (love.current.innerText = '<3')}
+    >&lt;3
+    </Box >
+  )
+}
 
 const NavSidebar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   const btnRef = React.useRef()
+  const [searchText, setSearchText] = React.useState('')
+
+  const filteredTools = tools.filter(tool => {
+    return tool.name.toLowerCase().includes(searchText.toLowerCase())
+  })
+
 
   return (
     <>
@@ -34,14 +56,38 @@ const NavSidebar = () => {
           <DrawerHeader>Search tools</DrawerHeader>
 
           <DrawerBody>
-            <Input placeholder='Type here...' />
+            <Input placeholder='Type here...' onChange={(e) => setSearchText(e.target.value)} />
+            <VStack spacing={4} align='stretch' mt={4}>
+              {
+                // Blue links for tools that match the search text
+                filteredTools.map(tool => (
+                  <Link key={tool.name} href={tool?.link || ""}>
+                    <Button
+                      width='100%'
+                      colorScheme='teal'
+                      variant={"link"}
+                      // rightIcon={<ExternalLinkIcon />}
+                      onClick={onClose}
+                    >
+                      {tool.name}
+                    </Button>
+                  </Link>
+                ))
+              }
+              {
+                filteredTools.length === 0 &&
+                <Box>No tools found</Box>
+              }
+            </VStack>
           </DrawerBody>
 
           <DrawerFooter>
-            <Button variant='outline' mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme='blue'>Save</Button>
+            Made with&nbsp;<Love />&nbsp;by&nbsp;<Link
+              style={{
+                color: 'blue',
+                textDecoration: 'underline',
+              }}
+              href='https://www.linkedin.com/in/kqwq/'>Kyle Wells</Link>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
